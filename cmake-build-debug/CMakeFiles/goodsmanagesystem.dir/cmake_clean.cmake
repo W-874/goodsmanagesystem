603f546240fce45c116dec2ebf9218ddbdd02267
W-874 <1317825684@qq.com>
2020-12-21 20:26:29 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/goodsmanagesystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goodsmanagesystem.dir/实验/main.c.o"
+  "goodsmanagesystem"
+  "goodsmanagesystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/goodsmanagesystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
