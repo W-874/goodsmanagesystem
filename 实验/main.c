d975af0b45f099310a4c106b5125c08dfa9c31ff
W-874 <1317825684@qq.com>
2021-01-04 22:48:09 +0800

    bug（

--- 实验/main.c
+++ 实验/main.c
@@ -32,7 +32,7 @@ typedef struct node
 //全局变量，存储当前商品的数量
 int CurrentCnt;
 
-GoodsInfo read_goods_info();
+GoodsInfo read_goods_info(void);
 void init_list(GoodsList **pL);
 void destory_list(GoodsList **pL);
 void destory_list_and_file(GoodsList **pL);
@@ -105,11 +105,11 @@ int main(void)
                 break;
             case 3:
                 item = read_goods_info();
-                int b = 0;
-
+                int pos;
                 printf("输入数字表明你要插入的商品位置：0.商品列表尾部 1.商品列表头部 i.商品列表中间第i号位置\n");
-                printf("debug");
-                insert_item(goodsList, item, b);
+                scanf("%d", &pos);
+                /* 补充代码insert_item调用*/
+                insert_item(goodsList, item, pos);
                 break;
             case 4:
                 printf("输入要删除记录的 ID：");
@@ -143,38 +143,38 @@ int main(void)
 /**********************************************************
  * init_list
  **********************************************************/
-void init_list(GoodsList **L)
+void init_list(GoodsList** L)
 {
-    FILE *fp;
+    FILE* fp;
     GoodsInfo goodsInfo;
     GoodsList *p, *r;
 
-    (*L) = (GoodsList *)malloc(sizeof(GoodsList));
+    CurrentCnt = 0;
+    (*L) = (GoodsList*)malloc(sizeof(GoodsList));
     r = (*L);
-    if ((fp = fopen(GOODS_FILE_NAME, "r")) == NULL)
-    {
-        if ((fp = fopen(GOODS_FILE_NAME, "w")) == NULL)
-        {
-            printf("提示：不能创建商品文件\n");
-        }
+    if (r == NULL) {
+        return;
     }
-    else
-    {
-        while (!feof(fp))
-        {
+    if ((fp = fopen(GOODS_FILE_NAME, "r")) == NULL) {
+        if ((fp = fopen(GOODS_FILE_NAME, "w")) == NULL)
+            printf("提示：不能创建商品文件\n");
+    } else {
+        while (!feof(fp)) {
             fscanf(fp, "%s", goodsInfo.goods_id);
             fscanf(fp, "%s", goodsInfo.goods_name);
             fscanf(fp, "%d", &goodsInfo.goods_price);
             fscanf(fp, "%s", goodsInfo.goods_discount);
             fscanf(fp, "%d", &goodsInfo.goods_amount);
-            fscanf(fp, "%d", &goodsInfo.goods_remain);
-            //仿写fscanf完成goodsInfo其他部分的内容
+            fscanf(fp, "%d\t", &goodsInfo.goods_remain);
+            /* 仿写fscanf完成goodsInfo其他部分的内容*/
             p = (GoodsList*)malloc(sizeof(GoodsList));
-            /* 完成GoodsList的构建*/
-            p->data=goodsInfo;
-            p->next=NULL;
+            if (p == NULL) {
+                return;
+            }
+            p->data = goodsInfo;
+            p->next = NULL;
             r->next = p;
-            r=r->next;
+            r = p;
             CurrentCnt++;
         }
     }
@@ -191,31 +191,36 @@ bool insert_item(GoodsList *L, GoodsInfo goodsInfo, int choice)
 {
     GoodsList *temp;
     GoodsList *pre = L, *p = L->next;
+    temp = malloc(sizeof(GoodsList));
     int i;
     if (CurrentCnt >= 100)
     {
         printf("信息库已满，要插入请先删除一定量的商品数据!\n");
         return false;
     }
-
+    if (temp == NULL) {
+        return false;
+    }
+    temp -> data = goodsInfo;
+    temp -> next = NULL;
     switch (choice)
     {
         case 0:
             //尾插法插入新商品
-            printf("W-874");
-            for (;p != NULL;p = p->next) {
-                if (p->next == NULL) {
-                    printf("debug");
-                    temp->data = goodsInfo;
-                    temp->next = NULL;
-                    p->next=temp;
+            if (pre == NULL)
+                return false;
+            else {
+                for (; p != NULL; p = p->next) {
+                    if (p->next == NULL) {
+                        p->next = temp;
+                        CurrentCnt++;
+                    }
                 }
             }
             return true;
         case 1:
             //头插法插入新商品
             /* 补充代码*/
-            printf("DEBUG");
             temp->data = goodsInfo;
             temp->next = pre->next;
             pre->next = temp;
@@ -261,6 +266,7 @@ bool delete_item(GoodsList *L, char* goods_id) {
             if (strcmp(p->data.goods_id, goods_id) == 0) {
                 pre->next = p->next;
                 free(p);
+                CurrentCnt --;
                 return true;
             }
         }
@@ -290,63 +296,72 @@ GoodsList* search_item(GoodsList *L, char* goods_id)
 /**********************************************************
  * change_item
  **********************************************************/
-bool change_item(GoodsList *L, char* goods_id, GoodsInfo new_info)
+bool change_item(GoodsList* L, char* goods_id, GoodsInfo new_info)
 {
-    GoodsList *p = L->next;
-    for (;p != NULL ; p = p->next) {
-        if (strcmp(p->data.goods_id, goods_id) == 0){
+    GoodsList* p = L->next;
+
+    for (; p != NULL; p = p->next) {
+        if (strcmp(p->data.goods_id, goods_id) == 0) {
             p->data = new_info;
-
+            return true;
         }
     }
-    return true;
+    if (p != NULL) {
+        return false;
+    }
 }
 
-
-void output_one_item(GoodsList *p)
+void output_one_item(GoodsList* p)
 {
-    printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-    printf("ID:%-s\t 商品名称:%s\t 商品价格:%d\t 商品折扣:%s\t 商品数量:%d\t 商品余量:%d\n",p->data.goods_id, p->data.goods_name, p->data.goods_price, p->data.goods_discount, p->data.goods_amount, p->data.goods_remain);
-    printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-
-}
-
-
-
-void output_all_items(GoodsList *L)
-{
-    GoodsList *p = L;
     if (p == NULL) {
-        printf("无商品记录!\n");
+        printf("error");
+        return;
     }
-    else {
-        printf("显示所有商品:\n");
-        for (; p != NULL ; p = p -> next)
-        {
-            printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-            printf("ID:%-s\t 商品名称:%s\t 商品价格:%d\t 商品折扣:%s\t 商品数量:%d\t 商品余量:%d\n",p->data.goods_id, p->data.goods_name, p->data.goods_price, p->data.goods_discount, p->data.goods_amount, p->data.goods_remain);
-        }
-        printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    printf("discount:%s\n", p->data.goods_discount);
+    printf("id:%s\n", p->data.goods_id);
+    printf("name:%s\n", p->data.goods_name);
+    printf("remain:%d\n", p->data.goods_remain);
+    printf("price:%d\n", p->data.goods_price);
+    printf("amount:%d\n", p->data.goods_amount);
 
+    /* 补充代码*/
+}
+
+
+void output_all_items(GoodsList* L)
+{
+    if (L == NULL) {
+        return;
+    }
+    int i = 0;
+    GoodsList* node = L->next;
+    while (node != NULL) {
+        i++;
+        printf("第%d件：", i);
+        output_one_item(node);
+        printf("\n");
+        node = node->next;
     }
 }
 
 /**********************************************************
  * destory_list
  **********************************************************/
-void destory_list(GoodsList **L)
+void destory_list(GoodsList** L)
 {
     if (L == NULL || *L == NULL)
-    {
         return;
-    }
     GoodsList *pre = *L, *p = (*L)->next;
-    while (p != NULL){
-        pre -> next = p -> next;
+    GoodsList* ptemp = NULL;
+    while (p != NULL) {
+        ptemp = p->next;
         free(p);
-        p=pre -> next;
+        p = ptemp;
     }
     free(*L);
+    *L = NULL;
+    L = NULL;
+    CurrentCnt = 0;
 }
 
 /**********************************************************
@@ -358,6 +373,7 @@ void destory_list_and_file(GoodsList **L)
     destory_list(L);
     remove(GOODS_FILE_NAME);
 }
+
 /**********************************************************
  * save_to_file
  **********************************************************/
